2a551072dda644ad8169588b6d4342904f436913
anikroy61198 <129795927+anikroy61198@users.noreply.github.com>
2023-04-04 02:26:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,36 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import openai
 
-"""
-# Welcome to Streamlit!
+# Define API key and endpoint URL
+API_KEY = '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'
+#ENDPOINT = 'https://api.openai.com/v1/engines/davinci-codex/completions'
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Set up OpenAI API
+openai.api_key = API_KEY
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Define function to generate response from API
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine='davinci-codex',
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text.strip()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Define Streamlit app
+def app():
+    st.title('ChatGPT Demo')
 
+    # Allow user to enter a prompt
+    prompt = st.text_input('Enter a prompt:', '')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # Generate response when user clicks button
+    if st.button('Generate'):
+        response = generate_response(prompt)
+        st.text_area('Response:', value=response, height=200)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == '__main__':
+    app()
